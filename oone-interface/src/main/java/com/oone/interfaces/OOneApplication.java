f6e8d3dc746d916a116020cd33f2f6b1b8b59a44
songhyeonpk <ehftozl1997@gmail.com>
2026-03-19 14:20:32 +0900

    chore: 프로젝트 초기 설정 (#1)
    * 멀티 모듈 환경 구성 (gradle 설정)

---
+++ oone-interface/src/main/java/com/oone/interfaces/OOneApplication.java
@@ -0,0 +1,12 @@
+package com.oone.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.oone")
+public class OOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OOneApplication.class, args);
+    }
+}
